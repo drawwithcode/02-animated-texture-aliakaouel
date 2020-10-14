2f8a6f6bdc7f7e2529b8a1c33ad95a78e1576ae0
aliakaouel <31700227+aliakaouel@users.noreply.github.com>
2020-10-14 03:48:00 +0200

    assignment2

--- sketch.js
+++ sketch.js
@@ -2,11 +2,32 @@ function preload(){
   // put preload code here
 }
 
+let i = 30
+let j = i + 10
+const mycolors = ["powderblue", "sandybrown", "palegreen"]
+
 function setup() {
   createCanvas(windowWidth,windowHeight)
+  background("black")
   // put setup code here
 }
 
 function draw() {
-  // put drawing code here
+  let c = random(10, i);
+  let myc= random(mycolors);
+
+  for(let x=j; x<(width-j); x+=i){
+    for(let y=j; y<(height-j); y+=i){
+      fill(color(myc));
+      ellipse(x, y, c);
+
+      if(mouseIsPressed){
+    strokeWeight(4);
+    ellipse(x, y, c+20);
+  }
+    else {
+      strokeWeight(1);
+    }
+    }
+  }
 }
